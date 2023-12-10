64321b81e2df2ad3686fc4e34274723f0a0ec026
D0IP <69601196+D0IP@users.noreply.github.com>
2023-12-09 21:01:46 -0600

    door sfx (with godot audio)

--- scripts/door.gd
+++ scripts/door.gd
@@ -17,6 +17,7 @@ func use_door():
 	Globals.levels[level_to].enter(position_delta)
 	
 	Globals.levels[level_to].player.position = Globals.doors[door_connected].position
-	#FMODRuntime.play_one_shot_path("event:/Doord")
 	
-	#$StudioEventEmitter3D.play()
+	#play door sound
+	if $AudioStreamPlayer.playing == false:
+		$AudioStreamPlayer.play()
