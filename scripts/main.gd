66321c7e7e4cd6d3dc080d4a2db94df55e8d17d8
D0IP <69601196+D0IP@users.noreply.github.com>
2023-12-11 15:06:21 -0600

    better positioning of listener and change to global panning strength

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @onready var camera = $Camera3D
-@onready var listener = $AudioListener3D
+@onready var listener = $Camera3D/AudioListener3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +18,9 @@ func _ready():
 	Globals.level_changed.connect(level_changed)
 	Globals.levels[Globals.LEVELS.LEVEL1].enter()
 	
+	#set listener z offset
+	listener.global_position.z = Globals.cal_3d_pos(Globals.current_level, Globals.player_pos).z + 0.1
+	
 func _process(delta):
 	update_camera()
 
@@ -30,10 +33,12 @@ func level_changed():
 			level.sprite.modulate.a = factor
 		else:
 			level.sprite.modulate.a = factor
-		
+	
+	#set listener z offset
+	listener.global_position.z = Globals.cal_3d_pos(Globals.current_level, Globals.player_pos).z + 0.1
 func update_camera():
 	level_changed()
 	camera.global_position = Globals.cal_3d_pos(Globals.current_level, Globals.player_pos)
 	camera.global_position.z += Globals.camera_distance
 	
-	listener.global_position = Globals.cal_3d_pos(Globals.current_level, Globals.player_pos)
+	#listener.global_position = Globals.cal_3d_pos(Globals.current_level, Globals.player_pos)
