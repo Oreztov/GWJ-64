0e7a7a99472ac8cb43320f58325c6f6f99f9f14d
D0IP <69601196+D0IP@users.noreply.github.com>
2023-12-15 13:38:40 -0600

    footsteps work

--- scripts/player.gd
+++ scripts/player.gd
@@ -84,5 +84,5 @@ func _on_area_2d_area_exited(area):
 func _on_animated_player_sprite_frame_changed():
 	var frame = %AnimatedPlayerSprite.frame
 	if %AnimatedPlayerSprite.animation == "Walk":
-		if frame == 0 or frame == 4:
-			print("step")
+		if frame == 1 or frame == 5:
+			$sfxFootstep.play()
