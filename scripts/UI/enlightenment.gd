bc8f308835b50ef096c0cb9a3f7b78ba705398f9
D0IP <69601196+D0IP@users.noreply.github.com>
2023-12-17 12:13:51 -0600

    better enlightenment sound crossfading

--- scripts/UI/enlightenment.gd
+++ scripts/UI/enlightenment.gd
@@ -30,13 +30,19 @@ func update():
 	%ENLIGHTENMENT.modulate.a = ratio + 0.25
 	if linear_to_db(value) > 0:
 		var volume_sfx = create_tween()
-		volume_sfx.tween_property($sfxEnlightenment, "volume_db", linear_to_db(ratio), 300/1000)
 		var volume_light_music = create_tween()
-		volume_light_music.tween_property($musicLight, "volume_db", linear_to_db(ratio), 300/1000)
 		var volume_main_music = create_tween()
-		volume_main_music.tween_property($musicMain, "volume_db", linear_to_db(-1*ratio+1), 300/1000)
 		var volume_ambience = create_tween()
-		volume_ambience.tween_property($streetAmbience, "volume_db", linear_to_db(-1*ratio+1), 300/1000)
+		if linear_to_db(value) < 50:
+			volume_sfx.tween_property($sfxEnlightenment, "volume_db", 2*linear_to_db(ratio), 300/1000)
+			volume_light_music.tween_property($musicLight, "volume_db", 2*linear_to_db(ratio), 300/1000)
+			volume_main_music.tween_property($musicMain, "volume_db", 2*linear_to_db(-1*ratio+1), 300/1000)
+			volume_ambience.tween_property($streetAmbience, "volume_db", 2*linear_to_db(-1*ratio+1), 300/1000)
+		if linear_to_db(value) > 50:
+			volume_sfx.tween_property($sfxEnlightenment, "volume_db", 1, 300/1000)
+			volume_light_music.tween_property($musicLight, "volume_db", 1, 300/1000)
+			volume_main_music.tween_property($musidddcMain, "volume_db", 0, 300/1000)
+			volume_ambience.tween_property($streetAmbience, "volume_db", 0, 300/1000)
 func set_objective(obj: Globals.OBJECTIVES):
 	%Objective.text = Globals.objectives[obj]
 	%GPUParticles2D.emitting = true
